849e11f553bbce8ed174f4d2ff176737a311dd30
lukas <lukas@ibb.waw.pl>
2020-06-02 15:24:27 +0200

    Build script (UX) links script to Range Cards directory

--- Range Cards/range_card_parser/build.sh
+++ Range Cards/range_card_parser/build.sh
@@ -2,6 +2,7 @@
 #Run windows guil CPAN client
 #> get pp
 #> install pp
+ln -sf ../range_card_parser.pl src/range_card_parser.pl
 \opt\Perl\perl\site\bin\pp -o ../range_card_parser.exe src/range_card_parser.pl
 
 
