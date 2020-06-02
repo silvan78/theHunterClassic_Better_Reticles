5565e7ebb7c747bbc644d365e639f1ea6ce80bc3
lukas <lukas@ibb.waw.pl>
2020-06-02 13:14:58 +0200

    build.bat updated to include search for installed pp.bat

--- Range Cards/range_card_parser/build.bat
+++ Range Cards/range_card_parser/build.bat
@@ -1,4 +1,4 @@
-c:\opt\Perl\perl\site\bin\pp.bat -o ..\range_card_parser.exe src\range_card_parser.pl
+:: @echo off
+FOR /F "tokens=* USEBACKQ" %%g IN (`where.exe pp.bat`) do (SET "PPPATH=%%g")
 
-
-
+%pppath% -o ../range_card_parser.exe src/range_card_parser.pl
