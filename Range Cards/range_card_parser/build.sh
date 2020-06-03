647d0a9b5812cf23937b651a842e06196f18241f
silvan78 <lukasz.knizewski@gmail.com>
2020-06-03 21:39:08 +0200

    reorganized perl src directory and update build.{sh,bat} instructions

--- Range Cards/range_card_parser/build.sh
+++
@@ -1,8 +0,0 @@
-# Install Strawberry Perl
-#Run windows guil CPAN client
-#> get pp
-#> install pp
-ln -sf ../range_card_parser.pl src/range_card_parser.pl
-\opt\Perl\perl\site\bin\pp -o ../range_card_parser.exe src/range_card_parser.pl
-
-
